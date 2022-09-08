a271e20050d5fac35d24db30f7a8b1c03d9af580
Hanna Fu <fuh1836college@gmail.com>
2022-09-08 17:40:34 -0500

    slightly confused

--- HannaFu-Studio1/ViewController.swift
+++ HannaFu-Studio1/ViewController.swift
@@ -8,13 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var Text: UILabel!
 
+    @IBOutlet weak var Slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("hello world")
+        print("hello world hello world")
+        print("i want to death")
     }
-
 
 }
 
